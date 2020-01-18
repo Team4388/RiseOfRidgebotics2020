4568e31036cd97b8a0fbcf75284094060e001bc2
Keenan D. Buckley <keenandbuckley@protonmail.com>
2020-01-18 15:27:33 -0700

    Change Teleop Neutral Mode to Brake

--- src/main/java/frc4388/robot/Robot.java
+++ src/main/java/frc4388/robot/Robot.java
@@ -97,7 +97,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    m_robotContainer.setDriveNeutralMode(NeutralMode.Coast);
+    m_robotContainer.setDriveNeutralMode(NeutralMode.Brake);
 
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
