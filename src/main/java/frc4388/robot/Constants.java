9787998778af1ce5c765b18aaba9fbb6a126bfd6
ryan123rudder <42309874+ryan123rudder@users.noreply.github.com>
2020-03-07 13:22:19 -0700

    PID fix and Idle

--- src/main/java/frc4388/robot/Constants.java
+++ src/main/java/frc4388/robot/Constants.java
@@ -122,8 +122,7 @@ public final class Constants {
         public static final int SHOOTER_SLOT_IDX = 0;
         public static final int SHOOTER_PID_LOOP_IDX = 1;
         public static final int SHOOTER_TIMEOUT_MS = 30;
-        public static final Gains DRUM_SHOOTER_GAINS = new Gains(0.4, 0.0005, 13, 0.05, 0, 1.0);
-        //public static final Gains DRUM_SHOOTER_GAINS = new Gains(0.0, 0.0, 0.0, 0.05, 0, 1.0);
+        public static final Gains DRUM_SHOOTER_GAINS = new Gains(0.55, 0.0, 100, 0.0, 0, 1.0);
         public static final Gains SHOOTER_TURRET_GAINS = new Gains(0.0, 0.0, 0.0, 0.0, 0, 1.0);
         public static final Gains SHOOTER_ANGLE_GAINS = new Gains(0.05, 0.0, 0.0, 0.0, 0, 0.3);
         public static final double SHOOTER_TURRET_MIN = -1.0;
