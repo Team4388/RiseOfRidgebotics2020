9e59e34314d0b22aefd41421ae960ad7954f1207
ryan123rudder <42309874+ryan123rudder@users.noreply.github.com>
2020-03-09 20:06:30 -0600

    Remove Old Unusable Commands

--- src/main/java/frc4388/robot/RobotContainer.java
+++ src/main/java/frc4388/robot/RobotContainer.java
@@ -38,8 +38,6 @@ import frc4388.robot.commands.drive.DriveWithJoystick;
 import frc4388.robot.commands.drive.TurnDegrees;
 import frc4388.robot.commands.intake.RunIntakeWithTriggers;
 import frc4388.robot.commands.shooter.CalibrateShooter;
-import frc4388.robot.commands.shooter.ShootFireGroup;
-import frc4388.robot.commands.shooter.ShootFullGroup;
 import frc4388.robot.commands.shooter.ShootPrepGroup;
 import frc4388.robot.commands.shooter.TrackTarget;
 import frc4388.robot.commands.shooter.TrimShooter;
